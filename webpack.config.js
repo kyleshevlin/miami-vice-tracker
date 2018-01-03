e5065d308e0e7e12e647e3b6d0fbe6a577bc3da1
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-01-02 19:46:43 -0800

    final pieces to get it deployable

--- webpack.config.js
+++ webpack.config.js
@@ -61,10 +61,7 @@ module.exports = env => {
       ]
     },
     plugins: [
-      new Dotenv({
-        path: './.env',
-        safe: false
-      }),
+      new Dotenv(),
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify(
           process.env.NODE_ENV || 'development'
