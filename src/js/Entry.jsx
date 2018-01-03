e2631c5358eeb47adf4873816c838ef1064ade01
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-01-03 08:39:02 -0800

    more GA setup

--- src/js/Entry.jsx
+++ src/js/Entry.jsx
@@ -5,4 +5,6 @@ import App from './components/App'
 import '../scss/app.scss'
 
 ReactGA.initialize(process.env.GA_ID)
+ReactGA.pageview(window.location.pathname + window.location.search)
+
 ReactDOM.render(<App />, document.getElementById('app'))
