be43095e25bb432dda5762ca78f83d8df5ba320c
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-01-02 23:27:14 -0800

    add google analytics

--- src/js/Entry.jsx
+++ src/js/Entry.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import App from './components/App'
-
 import '../scss/app.scss'
 
+ReactGA.initialize(process.env.GA_ID)
 ReactDOM.render(<App />, document.getElementById('app'))
